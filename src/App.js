c0831bc2f39a92597f341123ebfb57252976544c
Ian Rones <ronesp.85@gmail.com>
2020-02-11 11:32:04 -0800

    [add] header component built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Heading from './Heading'
+import Heading from './Heading';
+import PageHeader from './PageHeader'
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <PageHeader />
       <Heading title="Hello World" subtitle= "Foo Bar"/>
-      
     </div>
   );
 }
---
+++ src/PageHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PageHeader.css'
+
+function PageHeader() {
+    return (
+        <div className="PageHeader">
+            <header>
+                <h1>Candy Landing Page</h1>
+            </header>
+        </div>
+    )
+}
+
+export default PageHeader
